625c76cdd211ce38cd5bc8a0a02b79eb6f343830
shushanth <sushanth179@gmail.com>
2023-07-28 22:39:41 +0530

    Initialized Spring Boot App

---
+++ src/main/java/com/cricFizzAlerts/CricFizzAlertsApplication.java
@@ -0,0 +1,13 @@
+package com.cricFizzAlerts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CricFizzAlertsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CricFizzAlertsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cricFizzAlerts/CricFizzAlertsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cricFizzAlerts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CricFizzAlertsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
